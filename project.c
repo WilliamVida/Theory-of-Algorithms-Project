a6b85a2e817412b5000ae9cdf57662ddf3ab04ba
WilliamVida <williamvidaisgod@gmail.com>
2021-03-07 16:21:51 +0000

    Changed functions.

--- project.c
+++ project.c
@@ -1,97 +1,91 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-// const uint64_t K[80] = {
-//     428a2f98d728ae22 7137449123ef65cd b5c0fbcfec4d3b2f e9b5dba58189dbbc
-//     3956c25bf348b538 59f111f1b605d019 923f82a4af194f9b ab1c5ed5da6d8118
-//     d807aa98a3030242 12835b0145706fbe 243185be4ee4b28c 550c7dc3d5ffb4e2
-//     72be5d74f27b896f 80deb1fe3b1696b1 9bdc06a725c71235 c19bf174cf692694
-//     e49b69c19ef14ad2 efbe4786384f25e3 0fc19dc68b8cd5b5 240ca1cc77ac9c65
-//     2de92c6f592b0275 4a7484aa6ea6e483 5cb0a9dcbd41fbd4 76f988da831153b5
-//     983e5152ee66dfab a831c66d2db43210 b00327c898fb213f bf597fc7beef0ee4
-//     c6e00bf33da88fc2 d5a79147930aa725 06ca6351e003826f 142929670a0e6e70
-//     27b70a8546d22ffc 2e1b21385c26c926 4d2c6dfc5ac42aed 53380d139d95b3df
-//     650a73548baf63de 766a0abb3c77b2a8 81c2c92e47edaee6 92722c851482353b
-//     a2bfe8a14cf10364 a81a664bbc423001 c24b8b70d0f89791 c76c51a30654be30
-//     d192e819d6ef5218 d69906245565a910 f40e35855771202a 106aa07032bbd1b8
-//     19a4c116b8d2d0c8 1e376c085141ab53 2748774cdf8eeb99 34b0bcb5e19b48a8
-//     391c0cb3c5c95a63 4ed8aa4ae3418acb 5b9cca4f7763e373 682e6ff3d6b2b8a3
-//     748f82ee5defb2fc 78a5636f43172f60 84c87814a1f0ab72 8cc702081a6439ec
-//     90befffa23631e28 a4506cebde82bde9 bef9a3f7b2c67915 c67178f2e372532b
-//     ca273eceea26619c d186b8c721c0c207 eada7dd6cde0eb1e f57d4f7fee6ed178
-//     06f067aa72176fba 0a637dc5a2c898a6 113f9804bef90dae 1b710b35131c471b
-//     28db77f523047d84 32caab7b40c72493 3c9ebe0a15c9bebc 431d67c49c100d4c
-//     4cc5d4becb3e42b6 597f299cfc657e2a 5fcb6fab3ad6faec 6c44198c4a475817
-// };
+#define W 64
 
-// const uint64_t H[8] = {
-//     6a09e667f3bcc908,
-//     bb67ae8584caa73b,
-//     3c6ef372fe94f82b,
-//     a54ff53a5f1d36f1,
-//     510e527fade682d1,
-//     9b05688c2b3e6c1f,
-//     1f83d9abfb41bd6b,
-//     5be0cd19137e2179
-// };
+// SHA-512 operations.
+// Section 2.2.2 and pages 5-6 of the Secure Hash Standard.
+#define ROTL(x, n) ((x << n) | (x >> (W - n)))
+#define ROTR(x, n) ((x >> n) | (x << (W - n)))
+#define SHR(x, n) (x >> n)
 
-uint64_t Ch(uint64_t x, uint64_t y, uint64_t z)
-{
-    return ((x & y) ^ (~x & z));
-}
+// SHA-512 functions.
+// Section 4.1.3 and page 11 of the Secure Hash Standard.
+#define CH(x, y, z) ((x & y) ^ (~x & z))
+#define MAJ(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
+#define SIG0(x) (ROTR(x, 28) ^ ROTR(x, 34) ^ ROTR(x, 39))
+#define SIG1(x) (ROTR(x, 14) ^ ROTR(x, 18) ^ ROTR(x, 41))
+#define Sig0(x) (ROTR(x, 1) ^ ROTR(x, 8) ^ SHR(x, 7))
+#define Sig1(x) (ROTR(x, 19) ^ ROTR(x, 61) ^ SHR(x, 6))
 
-uint64_t Maj(uint64_t x, uint64_t y, uint64_t z)
-{
-    return ((x & y) ^ (x & z) ^ (y & z));
-}
+// SHA-512 constants consisting of 80 constant 64-bit words.
+// Section 4.2.3 and page 12 of the Secure Hash Standard.
+const uint64_t K[] = {
+    0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
+    0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
+    0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
+    0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694,
+    0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
+    0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,
+    0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4,
+    0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70,
+    0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df,
+    0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b,
+    0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30,
+    0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8,
+    0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8,
+    0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3,
+    0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
+    0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b,
+    0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178,
+    0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b,
+    0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c,
+    0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817};
 
-uint64_t shr(uint64_t x, uint64_t n)
-{
-    return (x >> n);
-}
-
-uint64_t rotr(uint64_t x, uint64_t n)
-{
-    return ((x >> n) | (x << 32 - n));
-}
-
-uint64_t sigmaUpper0(uint64_t x)
-{
-    return (rotr(x, 2) ^ rotr(x, 13) ^ rotr(x, 22));
-}
-
-uint64_t sigmaUpper1(uint64_t x)
-{
-    return (rotr(x, 6) ^ rotr(x, 11) ^ rotr(x, 25));
-}
-
-uint64_t sigmaLower0(uint64_t x)
-{
-    return (rotr(x, 7) ^ rotr(x, 18) ^ rotr(x, 3));
-}
-
-uint64_t sigmaLower1(uint64_t x)
-{
-    return (rotr(x, 17) ^ rotr(x, 19) ^ rotr(x, 10));
-}
+// SHA-512 initial hash values consisting of eight 64-bit words, in hex.
+// Section 5.3.5 and pages 15-16 of the Secure Hash Standard.
+const uint64_t H[] = {
+    0x6a09e667f3bcc908,
+    0xbb67ae8584caa73b,
+    0x3c6ef372fe94f82b,
+    0xa54ff53a5f1d36f1,
+    0x510e527fade682d1,
+    0x9b05688c2b3e6c1f,
+    0x1f83d9abfb41bd6b,
+    0x5be0cd19137e2179};
 
 void sha512()
 {
-    printf("SHA512\n");
+    printf("SHA-512\n");
+    printf("===============\n");
 }
 
 int main(int argc, char *argv[])
 {
-    uint64_t x = 0x0F0F0F0F;
-
     sha512();
 
-    printf("%" PRIX32 "\n", shr(x, 10));
-    printf("%" PRIX32 "\n", rotr(x, 10));
-    printf("%" PRIX32 "\n", sigmaUpper0(x));
-    printf("%" PRIX32 "\n", sigmaUpper1(x));
-    printf("%" PRIX32 "\n", sigmaLower0(x));
-    printf("%" PRIX32 "\n", sigmaLower1(x));
+    uint64_t x = 0xF1234567;
+    uint64_t y = 0x0A0A0A0A;
+    uint64_t z = 0xB0B0B0B0;
+
+    uint64_t ans;
+
+    ans = CH(x, y, z);
+    printf("CH(%08" PRIX64 ",%08" PRIX64 ",%08" PRIX64 ") = %08" PRIX64 "\n", x, y, z, ans);
+
+    ans = MAJ(x, y, z);
+    printf("MAJ(%08" PRIX64 ",%08" PRIX64 ",%08" PRIX64 ") = %08" PRIX64 "\n", x, y, z, ans);
+
+    printf("ROTL(%08" PRIX64 ") -> %08" PRIX64 "\n", x, ROTL(x, 4));
+    printf("ROTR(%08" PRIX64 ") -> %08" PRIX64 "\n", x, ROTR(x, 4));
+    printf("SHR(%08" PRIX64 ") -> %08" PRIX64 "\n", x, SHR(x, 4));
+    printf("SIG0(%08" PRIX64 " -> %08" PRIX64 "\n", x, SIG0(x));
+    printf("SIG1(%08" PRIX64 " -> %08" PRIX64 "\n", x, SIG1(x));
+    printf("Sig0(%08" PRIX64 " -> %08" PRIX64 "\n", x, Sig0(x));
+    printf("Sig1(%08" PRIX64 " -> %08" PRIX64 "\n", x, Sig1(x));
+
+    printf("K[0] = %08" PRIX64 "\tK[63] = %08" PRIX64 "\n", K[0], K[79]);
+    printf("H[0] = %08" PRIX64 "\tH[7] = %08" PRIX64 "\n", H[0], H[7]);
 
     return 0;
 }
